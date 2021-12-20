4ee67c8f1ed58af27600cd58a0b3dfe6eec806de
Kayalla <kayallapl@id.uff.br>
2021-12-20 09:10:37 -0300

    initializing streaming side

---
+++ streaming_side/main.py
@@ -0,0 +1,38 @@
+import cv2, imutils, socket, time, base64
+
+BUFF_SIZE = 65536
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+host_ip = 'ID_MAQUINA'
+port = 9999
+socket_address = (host_ip,port)
+server_socket.bind(socket_address)
+print('Escutando em: ',socket_address)
+
+vid = cv2.VideoCapture('videos/video1.mp4') #vem do client qual video reproduzir
+fps,st,frames_to_count,cnt = (0,0,20,0)
+
+while True:
+	msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
+	print('Conexão de: ',client_addr)
+	WIDTH=400
+	while(vid.isOpened()):
+		_,frame = vid.read()
+		frame = imutils.resize(frame,width=WIDTH)
+		encoded,buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+		message = base64.b64encode(buffer)
+		server_socket.sendto(message,client_addr)
+		frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+		cv2.imshow('Transmitindo vídeo',frame)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord('q'):
+			server_socket.close()
+			break
+		if cnt == frames_to_count:
+			try:
+				fps = round(frames_to_count/(time.time()-st))
+				st=time.time()
+				cnt=0
+			except:
+				pass
+		cnt+=1
