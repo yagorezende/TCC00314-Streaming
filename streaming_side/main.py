0e1566ba155d08f8e34e3b4d8ce7e28acbdb000b
Yago Rezende <yagorezende@id.uff.br>
2021-12-20 20:30:24 -0300

    reorder udp server

--- streaming_side/main.py
+++ streaming_side/main.py
@@ -1,38 +1,65 @@
+import json
+from os import listdir
+from os.path import isfile, join
 import cv2, imutils, socket, time, base64
 
 BUFF_SIZE = 65536
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
-host_ip = 'ID_MAQUINA'
-port = 9999
-socket_address = (host_ip,port)
-server_socket.bind(socket_address)
-print('Escutando em: ',socket_address)
+host_ip = 'localhost'
+port = 5050
+WIDTH = 400
 
-vid = cv2.VideoCapture('videos/video1.mp4') #vem do client qual video reproduzir
-fps,st,frames_to_count,cnt = (0,0,20,0)
 
-while True:
-	msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
-	print('Conexão de: ',client_addr)
-	WIDTH=400
-	while(vid.isOpened()):
-		_,frame = vid.read()
-		frame = imutils.resize(frame,width=WIDTH)
-		encoded,buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
-		message = base64.b64encode(buffer)
-		server_socket.sendto(message,client_addr)
-		frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-		cv2.imshow('Transmitindo vídeo',frame)
-		key = cv2.waitKey(1) & 0xFF
-		if key == ord('q'):
-			server_socket.close()
-			break
-		if cnt == frames_to_count:
-			try:
-				fps = round(frames_to_count/(time.time()-st))
-				st=time.time()
-				cnt=0
-			except:
-				pass
-		cnt+=1
+def open_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
+    socket_address = (host_ip, port)
+    server_socket.bind(socket_address)
+
+    print('Escutando em: ', socket_address)
+    running = True
+    while running:
+        msg, client_addr = server_socket.recvfrom(BUFF_SIZE)
+        print('Conexão de: ', client_addr)
+        request = json.loads(msg)
+        if request.get("request") == "LISTAR_VIDEOS":
+            list_videos(server_socket, client_addr)
+        elif request["request"] == "REPRODUZIR_VIDEO":
+            start_stream(server_socket, client_addr, width=WIDTH)
+        else:
+            break
+
+
+def list_videos(server_socket, client_addr):
+    onlyfiles = [f for f in listdir("videos") if isfile(join("videos", f))]
+    msg = {"request": "LISTA_DE_VIDEOS", "lista": onlyfiles}
+    server_socket.sendto(json.dumps(msg).encode(), client_addr)
+
+
+def start_stream(server_socket, client_addr, filename="videos/video1.mp4", width=400):
+    vid = cv2.VideoCapture(filename)  # vem do client qual video reproduzir
+    fps, st, frames_to_count, cnt = (0, 0, 20, 0)
+
+    while vid.isOpened():
+        _, frame = vid.read()
+        frame = imutils.resize(frame, width=width)
+        encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+        message = base64.b64encode(buffer)
+        server_socket.sendto(message, client_addr)
+        frame = cv2.putText(frame, 'FPS: ' + str(fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow('Transmitindo vídeo', frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            server_socket.close()
+            break
+        if cnt == frames_to_count:
+            try:
+                fps = round(frames_to_count / (time.time() - st))
+                st = time.time()
+                cnt = 0
+            except:
+                pass
+        cnt += 1
+
+
+if __name__ == "__main__":
+    open_server()
